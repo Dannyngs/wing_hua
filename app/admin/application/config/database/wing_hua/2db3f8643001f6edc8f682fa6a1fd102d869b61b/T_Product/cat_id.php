bc4de8b481f1f30a18c58fa71edd7551ba8aa93e
dannyng198811 <danny.ng198811@yahoo.com>
2016-02-22 16:27:16 +0800

    d

---
+++ app/admin/application/config/database/wing_hua/2db3f8643001f6edc8f682fa6a1fd102d869b61b/T_Product/cat_id.php
@@ -0,0 +1,2 @@
+<?php exit; ?>
+{"field":"cat_id","label":"cat_id","type":"text","type_options":{"size":"210","width":"300","height":"100","thumbnail":"mini"},"validation":"","db_options":{"table":"T_Category"},"list_choose":"database","multiple":"multiple"}
